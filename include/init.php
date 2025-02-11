0e38a3af5f2a8c79bd428d1db1fba878b1387d9c
28010-59-03 <28010-59-03@exchange.ad.afpanet>
2025-02-11 14:34:03 +0100

    1st version shop

---
+++ include/init.php
@@ -0,0 +1,39 @@
+<?php 
+// -------- Connexion BDD
+$dbConnect = new PDO("mysql:host=localhost;dbname=shop", "root", "", [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+]);
+// echo '<pre>'; print_r($dbConnect); echo '</pre>';
+
+// -------- SESSION
+
+session_start();
+
+// -------- CHEMIN
+// echo '<pre>'; print_r($_SERVER); echo '</pre>';
+
+define('RACINE_SITE', $_SERVER['DOCUMENT_ROOT'] . '/');
+// echo '<pre>'; print_r(RACINE_SITE); echo '</pre>';
+// Cette constante retourne le chemin physique du dossier htdocs sur le serveur
+// Lors de l'enregistrement d'image/photo, nous avons besoin du chemin complet du dossier
+// echo RACINE_SITE . 'Boutique/Shop/assets/images/nom_de_image' par exemple
+
+define("URL", "http://localhost/PHP/Boutique/Shop/");
+// <img url="URL . assets/images/images.jpg">
+// Cette constante servira à enregistrer l'URL d'une image dans la BDD. Dans la mesure où on ne peut pas enregistrer physiquement une image dans une BDD, on lui définit un chemin d'accès
+
+// -------- VARIABLES
+$content = '';
+
+// -------- FAILLES XSS
+foreach($_POST as $key => $value){
+    $_POST[$key] = htmlentities(addslashes($value));
+}
+foreach($_GET as $key => $value){
+    $_GET[$key] = htmlentities(addslashes($value));
+}
+
+// -------- INCLUSION FONCTIONS
+require_once("functions.php");
+?>
--- inscription.php
+++ inscription.php
@@ -1,5 +1,54 @@
 <?php 
-  require_once('include/header.php');
+require_once('include/init.php');
+
+// 1- controler la réception des données du formulaire
+echo '<pre>'; print_r($_POST); echo '</pre>';
+
+if(isset($_POST['submit'])){
+  if(empty($_POST['firstName'])){$errorFirstName = "Merci de renseigner votre prénom";}
+  if(empty($_POST['lastName'])){$errorLastName = "Merci de renseigner votre nom";}
+  if(empty($_POST['address'])){$errorAddress = "Merci de renseigner votre adresse";}
+  if(empty($_POST['city'])){$errorCity = "Merci de renseigner votre ville";}
+  if(empty($_POST['zipcode'])){$errorZipcode = "Merci de renseigner votre code postal";}
+
+  // 2- controler la disponibilité de l'email
+  $data = $dbConnect->prepare("SELECT COUNT(email) as nbMail FROM user WHERE email = :email");
+  $data->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
+  $data->execute();
+  $dispoMail = $data->fetch(PDO::FETCH_ASSOC);
+
+  if($dispoMail['nbMail']){$dispoEmail = false;}else{$dispoEmail = true;}
+
+  // 3- afficher un message d'erreur si le champ email est laissé vide
+  if(empty($_POST['email'])){
+    $erreurEmail = "Merci de renseigner une adresse email valide";
+  }else
+  // 4- controler la validité de l'email
+  if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+    $successEmail = true;
+  }else{$erreurEmail = "l'email n'est pas valide";}
+
+  // 5- afficher un message d'erreur si le mdp est laissé vide
+  if(empty($_POST['password'])){
+    $erreurPsw = 'Merci de rentrer un mot de passe';
+  }
+  if(empty($_POST['passwordConfirm'])){
+    $erreurPswRep = "Merci de confirmer votre mot de passe";
+  }
+
+  // force du mot de passe (au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial)
+  $regPsw = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/";
+  $pswStrength = preg_match($regPsw, $_POST["password"]);
+
+  // 6- controler que les champs mdp correspondent bien
+  if($_POST['password'] === $_POST['passwordConfirm']){
+    $validPsw = true;
+  }else{
+    $validPsw = false;
+  }
+}
+
+require_once('include/header.php');
 ?>
 
 
@@ -22,49 +71,76 @@
       <div class="row">
         <div class="col-lg-8 offset-lg-2">
           <div class="full">
-            <form action="">
+            <form method="post" action="">
               <fieldset>
+                <label for="firstName">Prénom</label>
+                <small class="text-danger"><?php if(isset($errorFirstName)) echo $errorFirstName; ?></small>
                 <input
                   type="text"
                   placeholder="Enter votre prénom"
-                  name="firstName"
-                  required />
+                  name="firstName" value="<?php if(isset($_POST['submit'])) echo $_POST['firstName'] ?>" />
+
+                <label for="lastName">Nom</label>
+                <small class="text-danger"><?php if(isset($errorLastName)) echo $errorLastName; ?></small>
                 <input
                   type="text"
                   placeholder="Enter votre nom"
-                  name="lastName"
-                  required />
+                  name="lastName" value="<?php if(isset($_POST['submit'])) echo $_POST['lastName'] ?>" />
+
+                <label for="email">Email</label>
+                <small class="text-danger"><?php if(isset($erreurEmail)){
+                    echo $erreurEmail;
+                  }elseif(isset($dispoEmail) && $dispoEmail == false){
+                    echo "Cet email est déjà pris";
+                  } ?></small>
                 <input
                   type="text"
                   placeholder="Entrez votre adresse e-mail"
-                  name="email"
-                  required />
+                  name="email" value="<?php if(isset($_POST['submit'])) echo $_POST['email'] ?>" />
+
+                <label for="address">Adresse</label>
+                <small class="text-danger"><?php if(isset($errorAddress)) echo $errorAddress; ?></small>
                 <input
                   type="text"
                   placeholder="Entrer votre adresse"
-                  name="address"
-                  required />
+                  name="address" value="<?php if(isset($_POST['submit'])) echo $_POST['address'] ?>" />
+
+                <label for="city">Ville</label>
+                <small class="text-danger"><?php if(isset($errorCity)) echo $errorCity; ?></small>
                 <input
                   type="text"
                   placeholder="Entrer votre ville"
-                  name="city"
-                  required />
+                  name="city" value="<?php if(isset($_POST['submit'])) echo $_POST['city'] ?>" />
+
+                <label for="zipcode">Code postal</label>
+                <small class="text-danger"><?php if(isset($errorZipcode)) echo $errorZipcode; ?></small>
                 <input
                   type="text"
                   placeholder="Entrer votre code postal"
-                  name="zipcode"
-                  required />
+                  name="zipcode" value="<?php if(isset($_POST['submit'])) echo $_POST['zipcode'] ?>" />
+
+                <label for="password">Mot de passe</label>
+                <small class="text-danger"><?php if(isset($erreurPsw)){
+                    echo $erreurPsw;
+                  }elseif(isset($pswStrength) && $pswStrength == false){
+                    echo "Le mot de passe doit comporter au moins 8 caractères, une minuscule, une majuscule, un chiffre et un caractère spécial";
+                  } ?></small>
                 <input
                   type="password"
-                  placeholder="Enter votre mot de passe"
-                  name="subject"
-                  required />
+                  placeholder="Entrer votre mot de passe"
+                  name="password" />
+
+                <label for="passwordConfirm">Confirmez votre mot de passe</label>
+                <small class="text-danger"><?php if(isset($erreurPswRep)){
+                    echo $erreurPswRep;
+                  }elseif(isset($validPsw) && $validPsw == false){
+                    echo "Les champs ne correspondent pas";} ?></small>
                 <input
-                  type="repeat_password"
+                  type="password"
                   placeholder="Répétez votre mot de passe"
-                  name="subject"
-                  required />
-                <input type="submit" value="Submit" />
+                  name="passwordConfirm" />
+
+                <input name="submit" type="submit" value="Submit" />
               </fieldset>
             </form>
           </div>
